3d455a5e835f844e55df903ea52561d30ae8cea0
Liam Dugan <ldugan@seas.upenn.edu>
2020-12-27 14:57:41 -0500

    Fixed markdown failing to render with proper style on blog posts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,8 +4,8 @@ import "typeface-merriweather"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
-import "./src/style.css"
 import "./src/bulma.css"
+import "./src/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
